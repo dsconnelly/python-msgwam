05648a94e8e74b617c3f2f5dcfc6689d662fc305
dsconnelly <dsconnelly@nyu.edu>
2023-11-02 09:52:44 -0400

    slight reworking of baseline experiment

--- experiments/baseline.py
+++ experiments/baseline.py
@@ -14,10 +14,6 @@ def save_reference_mean_state():
     ds.to_netcdf('data/baseline-mean.nc')
 
 def run_refinements():
-    config.n_day = 15
-    config.interactive_mean = False
-    config.mean_file = 'data/baseline-mean.nc'
-
     dr_init = config.dr_init * 4
     dm_init = config.dm_init * 4
     n_per_mode = config.n_per_mode // 4
@@ -33,9 +29,15 @@ def run_refinements():
         ds = integrate.SBDF2Integrator().integrate().to_dataset()
         ds.to_netcdf(f'data/baseline-{i ** 2}x.nc')
 
+    config.n_ray_max = n_ray_max
+    config.refresh()
+
+    ds = integrate.SBDF2Integrator().integrate().to_dataset()
+    ds.to_netcdf(f'data/baseline-1x-small.nc')
+
 def plot_errors():
     fig, ax = plt.subplots()
-    fig.set_size_inches(6, 4.5)
+    fig.set_size_inches(8, 6)
 
     with xr.open_dataset('data/baseline-16x.nc') as ds:
         days = ds['time'].values / (60 * 60 * 24)
@@ -49,8 +51,14 @@ def plot_errors():
         error = np.sqrt(((pmf - ref) ** 2).sum(axis=1))
         ax.plot(days, error, color=color, label=f'{i ** 2}x')
 
+    with xr.open_dataset(f'data/baseline-1x-small.nc') as ds:
+        pmf = ds['pmf_u'].values * 1000
+        
+    error = np.sqrt(((pmf - ref) ** 2).sum(axis=1))
+    ax.plot(days, error, color='darkviolet', label=f'1x (small)')
+
     ax.set_xlim(0, 15)
-    ax.set_ylim(0, 3.5)
+    ax.set_ylim(0, 12)
     ax.set_xticks(np.arange(16, step=3))
 
     ax.set_xlabel('time (days)')
@@ -59,11 +67,16 @@ def plot_errors():
 
     plt.tight_layout()
     ax.grid(True, color='lightgray')
-    plt.savefig('plots/baselines.png')
+    plt.savefig('plots/baseline.png', dpi=400)
 
 if __name__ == '__main__':
-    config.load_config('config/baselines.toml')
-    # save_reference_mean_state()
-    # run_refinements()
+    config.load_config('config/baseline.toml')
+    BASELINE = vars(config).copy()
+    save_reference_mean_state()
+
+    config.n_day = 15
+    config.interactive_mean = False
+    config.mean_file = 'data/baseline-mean.nc'
+
+    run_refinements()
     plot_errors()
-    
