numerical scheme was wrong (computing forward Euler with a rogue factor of 2 / 3) so I fixed and refactored a bit to make similar mistakes a bit harder to make

msgwam/integrate.py
@@ -5,13 +5,13 @@ from time import time as now
 from typing import Any, Optional
 
 import numpy as np
-import scipy as sp
 import tqdm
 import xarray as xr
 
-from scipy.linalg import lu_factor, lu_solve
+from scipy.linalg import lu_factor, lu_solve as _lu_solve
+lu_solve = lambda A, b: _lu_solve(A, b.T).T
 
-from . import config
+from . import config, sources
 from .mean import MeanFlow
 from .rays import RayCollection
 
@@ -23,18 +23,19 @@ class Integrator(ABC):
         """
 
         mean = MeanFlow()
-        rays = RayCollection(mean)
+        source_func = getattr(sources, config.source_type)
+        rays = RayCollection(source_func(mean))
 
         self.time = config.dt * np.arange(config.n_t_max)
         if not config.interactive_mean:
             with xr.open_dataset(config.mean_file) as ds:
                 ds = ds.interp(time=self.time)
-                self.prescribed_u = ds['u'].values
-                self.prescribed_v = ds['v'].values
+                u = ds['u'].values
+                v = ds['v'].values
 
-            mean.u = self.prescribed_u[0]
-            mean.v = self.prescribed_v[0]
-        
+            self.prescribed_wind = np.stack((u, v), axis=1)
+            mean.data = self.prescribed_wind[0]
+
         self.int_mean = [mean]
         self.int_rays = [rays]
 
@@ -99,20 +100,18 @@ class Integrator(ABC):
 
         start = now()
         for i in iterator:
+            rays.check_source()
             mean, rays = self.step(mean, rays)
             rays.check_boundaries(mean)
             rays.break_waves(mean)
 
             if not config.interactive_mean:
-                mean.u = self.prescribed_u[i]
-                mean.v = self.prescribed_v[i]
+                mean.data = self.prescribed_wind[i]
 
             if i % config.n_skip == 0:
                 self.int_mean.append(mean)
                 self.int_rays.append(rays)
-                
-                pmf = mean.pmf(rays, onto='centers')
-                self.int_pmf.append(pmf)
+                self.int_pmf.append(mean.pmf(rays, onto='centers'))
 
         self.runtime = now() - start
         
@@ -203,30 +202,35 @@ class SBDF2Integrator(Integrator):
     ) -> tuple[MeanFlow, RayCollection]:
         """Take an SBDF2 step, using semi-implicit Euler to initialize."""
         
-        du_dt, dv_dt = mean.dmean_dt(rays)
-        drays_dt = rays.drays_dt(mean)
+        dmean_dt = mean.d_dt(rays)
+        drays_dt = rays.d_dt(mean)
+        do_euler = self.last is None
+
+        if not do_euler:
+            idx = rays.age == 0
+            new = rays.data[:, idx].copy()
+
+            last_mean, last_rays = self.last
+            last_dmean_dt, last_drays_dt = self.dlast_dt
+
+        self.last = [mean.data, rays.data]
+        self.dlast_dt = [dmean_dt, drays_dt]
         mean, rays = copy(mean), copy(rays)
 
-        if self.last is None:
-            mean.u = lu_solve(self.A, mean.u + config.dt * du_dt)
-            mean.v = lu_solve(self.A, mean.v + config.dt * dv_dt)
+        if do_euler:
+            mean.data = lu_solve(self.A, (mean.data + config.dt * dmean_dt))
             rays.data = rays.data + config.dt * drays_dt
 
         else:
-            last_u, last_v, last_rays = self.last
-            last_du_dt, last_dv_dt, last_drays_dt = self.dlast_dt
+            mean.data = self.lhs(
+                mean.data, last_mean,
+                dmean_dt, last_dmean_dt,
+                stiff=True
+            )
 
-            idx = np.isnan(last_drays_dt[0])
-            last_rays[:, idx] = rays.data[:, idx]
-            last_drays_dt[:, idx] = 0
-
-            mean.u = self.lhs(mean.u, last_u, du_dt, last_du_dt, stiff=True)
-            mean.v = self.lhs(mean.v, last_v, dv_dt, last_dv_dt, stiff=True)
             rays.data = self.lhs(rays.data, last_rays, drays_dt, last_drays_dt)
+            rays.data[:, idx] = new + config.dt * drays_dt[:, idx]
 
-        self.last = [mean.u, mean.v, rays.data]
-        self.dlast_dt = [du_dt, dv_dt, drays_dt]
-        
         return mean, rays
 
     def lhs(
@@ -257,5 +261,5 @@ class SBDF2Integrator(Integrator):
         
         """
 
-        rhs = 2 * curr - 0.5 * last + config.dt* (2 * dcurr_dt - dlast_dt)
+        rhs = 2 * curr - 0.5 * last + config.dt * (2 * dcurr_dt - dlast_dt)
         return lu_solve(self.B, rhs) if stiff else 2 * rhs / 3

msgwam/mean.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-from copy import copy
 from typing import TYPE_CHECKING, Optional
 
 import numpy as np
@@ -12,60 +11,165 @@ if TYPE_CHECKING:
 class MeanFlow:
     def __init__(self) -> None:
         """
-        Initialize a MeanFlow using the configuration settings. Note that
-        config.load_config must have been called for initialization to succeed.
+        Initialize a MeanFlow using the settings set by config.load_config.
         """
 
         self.r_faces = np.linspace(*config.grid_bounds, config.n_grid)
         self.r_centers = (self.r_faces[:-1] + self.r_faces[1:]) / 2
         self.dr: float = self.r_faces[1] - self.r_faces[0]
 
-        self.rho = self.init_rho()
-        self.u, self.v = self.init_uv()
-        self.dp_dx, self.dp_dy = self.init_grad_p()
+        self.rho = self._init_rho()
+        self.data = self._init_uv()
+        self.grad_p = self._init_grad_p()
 
         nu_centers = config.mu / self.rho
         self.nu = np.interp(self.r_faces, self.r_centers, nu_centers)
         self.pmf_bar = np.zeros((2, *self.r_faces.shape))
 
-    def __add__(self, other: np.ndarray) -> MeanFlow:
+    @property
+    def u(self) -> np.ndarray:
         """
-        Return a new MeanFlow object sharing the same background profiles
-        (density and pressure gradients) but with velocities equal to those of
-        this object added to the data in other. Defined to make writing
-        time-stepping routines easier.
-
-        Parameters
-        ----------
-        other
-            Array of data to be added to the wind profiles (for example, the
-            calculated mean flow tendency multiplied by the time step).
+        Return the zonal component of the mean wind.
 
         Returns
         -------
-        MeanFlow
-            MeanFlow with updated wind profiles.
-
-        Raises
-        ------
-        ValueError
-            Indicates that other does not have the correct shape.
+        np.ndarray
+            Array of zonal wind velocities at cell centers.
 
         """
 
-        if other.shape != (2, len(self.r_centers)):
-            raise ValueError('other does not have correct shape')
-
-        output = copy(self)
-        output.u = self.u + other[0]
-        output.v = self.v + other[1]
-
-        return output
-
-    def init_rho(self) -> np.ndarray:
+        return self.data[0]
+    
+    @property
+    def v(self) -> np.ndarray:
         """
-        Initialize the mean density profile depending on whether or not the
-        Boussinesq approximation is made.
+        Return the meridional component of the mean wind.
+
+        Returns
+        -------
+        np.ndarray
+            Array of meridional wind velocities at cell centers.
+
+        """
+
+        return self.data[1]
+    
+    def project(
+        self,
+        rays: RayCollection,
+        data: np.ndarray,
+        onto: str
+    ) -> np.ndarray:
+        """
+        Project ray volume data onto a vertical grid.
+
+        Parameters
+        ----------
+        rays
+            Current ray volume properties.
+        data
+            Array of data to project (for example pseudomomentum fluxes). Should
+            have the same shape as the properties stored in `rays`.
+        onto
+            Whether to project onto cell 'faces' or cell 'centers'.
+
+        Returns
+        -------
+        np.ndarray
+            Projected values at each vertical grid point. Has the same length as
+            either `self.r_faces` or `self.r_centers`, depending on `onto`.
+
+        """
+
+        if config.proj_method == 'discrete':
+            edges = {'faces' : self.r_centers, 'centers' : self.r_faces}[onto]
+            output = np.nansum(self.get_fracs(rays, edges) * data, axis=1)
+
+            if onto == 'faces':
+                output = np.pad(output, 1, 'edge')
+
+            return output
+        
+        elif config.proj_method == 'gaussian':
+            sigma = rays.dr * config.smoothing
+            grid = {'faces' : self.r_faces, 'centers' : self.r_centers}[onto]
+            env = np.exp(-0.5 * ((rays.r - grid[:, None]) / sigma) ** 2)
+            amplitude = data * rays.dr / np.sqrt(2 * np.pi) / sigma
+
+            return np.nansum(amplitude * env, axis=1)
+        
+    def pmf(self, rays: RayCollection, onto: str='faces') -> np.ndarray:
+        """
+        Calculate the zonal and meridional pseudomomentum fluxes.
+
+        Parameters
+        ----------
+        rays
+            Current ray properties.
+        onto
+            Passed to `self.project` to indicate where to calculate the fluxes.
+            Can be either 'faces' (useful during integration) or 'centers' (for
+            saving offline diagnostics).
+
+        Returns
+        -------
+        np.ndarray
+            Array of shape (2, config.n_grid) or (2, config.n_grid - 1),
+            depending on the value of `onto`, whose rows correspond to the zonal
+            and meridional pseudomomentum fluxes, respectively.
+
+        """
+
+        action_flux = rays.cg_r() * rays.action
+        pmf_x = self.project(rays, action_flux * rays.k, onto=onto)
+        pmf_y = self.project(rays, action_flux * rays.l, onto=onto)
+
+        if config.shapiro_filter:
+            pmf_x[1:-1] = self._shapiro_filter(pmf_x)
+            pmf_y[1:-1] = self._shapiro_filter(pmf_y)
+
+        pmf = np.vstack((pmf_x, pmf_y))
+        if config.tau == 0:
+            return pmf
+        
+        pmf_bar = self.pmf_bar.copy()
+        if onto == 'centers':
+            pmf_bar = np.vstack((
+                np.interp(self.r_centers, self.r_faces, pmf_bar[0]),
+                np.interp(self.r_centers, self.r_faces, pmf_bar[1])
+            ))
+
+        ratio = config.dt / config.tau
+        return np.exp(-ratio) * (pmf_bar + ratio * pmf)
+    
+    def d_dt(self, rays: RayCollection) -> np.ndarray:
+        """
+        Calculate the time tendency of the mean wind, including Coriolis terms
+        and pseudomomentum flux divergences from the ray volumes.
+
+        Parameters
+        ----------
+        rays
+            Current ray properties.
+
+        Returns
+        -------
+        np.ndarray
+            Array whose first and second rows are the time tendencies of the
+            zonal and meridional wind, respectively, at cell centers.
+
+        """
+
+        self.pmf_bar = self.pmf(rays)
+        dpmf_dr = np.diff(self.pmf_bar, axis=1) / self.dr
+        coriolis = config.f0 * np.vstack((self.v, -self.u))
+
+        return coriolis - (self.grad_p + dpmf_dr) / self.rho
+
+    def _init_rho(self) -> np.ndarray:
+        """
+        Initialize the mean density profile depending on whether the Boussinesq
+        approximation is to be made.
 
         Returns
         -------
@@ -76,47 +180,38 @@ class MeanFlow:
 
         if config.boussinesq:
             return config.rhobar0 * np.ones(self.r_centers.shape)
-
+        
         return config.rhobar0 * np.exp(-self.r_centers / config.H_scale)
-
-    def init_uv(self) -> tuple[np.ndarray, np.ndarray]:
+    
+    def _init_uv(self) -> np.ndarray:
         """
         Initialize the mean wind profiles using the method identified in config.
 
         Returns
         -------
-        np.ndarray, np.ndarray
-            Mean u and v at cell centers.
+        np.ndarray
+            Array whose first row stores u and whose second row stores v.
 
         Raises
         ------
         ValueError
             Indicates that an unknown method was specified for initialization.
-            Currently, the only methods are 'sine_homogeneous' and 'gaussian'.
+            Currently, the only supported method is 'gaussian'.
 
         """
 
         method = config.uv_init_method
-        if method == 'sine_homogeneous':
-            tanh = np.tanh((self.r_centers - config.r0) / config.sig_r) + 1
-            sine = np.sin(2 * np.pi * self.r_centers / config.sig_r)
-
-            u = 0.5 * config.u0 * tanh * sine
-            v = np.zeros(u.shape)
-
-            return u, v
-
         if method == 'gaussian':
-            arg = -(((self.r_centers - config.r0) / config.sig_r) ** 2)
+            arg = -0.5 * (((self.r_centers - config.r0) / config.sig_r) ** 2)
             u = config.u0 * np.exp(arg)
             v = np.zeros(u.shape)
 
-            return u, v
-
+            return np.vstack((u, v))
+        
         message = f'Unknown method for initializing mean flow: {method}'
         raise ValueError(message)
-
-    def init_grad_p(self) -> np.ndarray:
+    
+    def _init_grad_p(self) -> np.ndarray:
         """
         Initialize the horizontal pressure gradients according to the
         geostrophic approximation.
@@ -133,149 +228,33 @@ class MeanFlow:
             self.rho * config.f0 * self.v,
             -self.rho * config.f0 * self.u
         ))
-
-    def project(
-        self,
-        rays: RayCollection,
-        data: np.ndarray,
-        onto: str
-    ) -> np.ndarray:
-        """
-        Project ray volume data onto a vertical grid.
-
-        Parameters
-        ----------
-        rays
-            Current ray volume properties.
-        data
-            The data to project (for example, pseudo-momentum fluxes). Should
-            have the same shape as the properties stored in rays.
-        onto
-            Whether to project onto cell 'faces' or cell 'centers'.
-
-        Returns
-        -------
-        np.ndarray
-            Projected values at each vertical grid point. Has the same length
-            as either `self.r_faces` or `self.r_centers`, depending on `onto`.
-
-        """
-
-        if config.proj_method == 'discrete':
-            grid = {'faces' : self.r_centers, 'centers' : self.r_faces}[onto]
-            output = np.nansum(self.get_fracs(rays, grid) * data, axis=1)
-
-            if onto == 'faces':
-                output = np.pad(output, 1, 'edge')
-
-            return output
-        
-        elif config.proj_method == 'gaussian':
-            sigma = rays.dr * config.smoothing
-            grid = {'faces' : self.r_faces, 'centers' : self.r_centers}[onto]
-            env = np.exp(-0.5 * ((rays.r - grid[:, None]) / sigma) ** 2)
-            amplitude = data * rays.dr / np.sqrt(2 * np.pi) / sigma
-
-            return np.nansum(amplitude * env, axis=1)
-
-    def pmf(self, rays: RayCollection, onto: str='faces') -> np.ndarray:
-        """
-        Calculate the zonal and meridional pseudomomentum fluxes.
-
-        Parameters
-        ----------
-        rays
-            RayCollection with current ray properties.
-        onto
-            Passed to `self.project` to indicate where to calculate the fluxes.
-            Can be either 'faces' (useful during integration) or 'centers' (for
-            saving offline diagnostics).
-
-        Returns
-        -------
-        np.ndarray
-            Array of shape (2, config.n_grid) whose rows correspond to the zonal
-            and meridional pseudomomentum fluxes, respectively, at cell faces.
-
-        """
-
-        action_flux = rays.cg_r() * rays.action / config.epsilon
-        pmf_x = self.project(rays, action_flux * rays.k, onto=onto)
-        pmf_y = self.project(rays, action_flux * rays.l, onto=onto)
-
-        if config.shapiro_filter:
-            pmf_x[1:-1] = self._shapiro_filter(pmf_x)
-            pmf_y[1:-1] = self._shapiro_filter(pmf_y)
-
-        pmf = np.vstack((pmf_x, pmf_y))
-        if config.tau == 0:
-            return pmf
-
-        pmf_bar = self.pmf_bar.copy()
-        if onto == 'centers':
-            pmf_bar = np.vstack((
-                np.interp(self.r_centers, self.r_faces, pmf_bar[0]),
-                np.interp(self.r_centers, self.r_faces, pmf_bar[1])
-            ))
-
-        ratio = config.dt / config.tau
-        return np.exp(-ratio) * (pmf_bar + ratio * pmf)
-
-    def dmean_dt(self, rays: RayCollection) -> np.ndarray:
-        """
-        Calculate the time tendency of the mean wind, including Coriolis terms
-        and pseudomomentum flux divergences from the ray volumes.
-
-        Parameters
-        ----------
-        rays
-            RayCollection with current ray properties.
-
-        Returns
-        -------
-        np.ndarray
-            Array whose first and second rows are the time tendencies of the
-            zonal and meridional wind, respectively, at cell centers.
-            
-        """
-
-        self.pmf_bar = self.pmf(rays)
-        dpmf_dr = np.diff(self.pmf_bar, axis=1) / self.dr
-        du_dt = config.f0 * self.v - (self.dp_dx + dpmf_dr[0]) / self.rho
-        dv_dt = -config.f0 * self.u - (self.dp_dy + dpmf_dr[1]) / self.rho
-
-        return np.vstack((du_dt, dv_dt))
     
     @staticmethod
-    def get_fracs(
-        rays: RayCollection,
-        grid: np.ndarray
-    ) -> np.ndarray:
+    def get_fracs(rays: RayCollection, edges: np.ndarray) -> np.ndarray:
         """
         Find the fraction of each grid cell that intersects each ray volume.
 
         Parameters
         ----------
         rays
-            Current ray volume properties.
-        grid
-            The edges of the vertical regions to project onto.
+            Current ray volume properties
+        edges
+            Edges of the vertical regions to project onto.
 
         Returns
         -------
-        np.ndarray
-            Fraction of each grid cell intersected by each ray. Has shape
-            (len(grid - 1), config.n_ray_max).
+        Fraction of each grid cell intersected by each ray volume. Has shape
+        (len(grid - 1), config.n_ray_max).
 
         """
 
         r_lo = rays.r - 0.5 * rays.dr
         r_hi = rays.r + 0.5 * rays.dr
 
-        r_mins = np.maximum(r_lo, grid[:-1, None])
-        r_maxs = np.minimum(r_hi, grid[1:, None])
+        r_mins = np.maximum(r_lo, edges[:-1, None])
+        r_maxs = np.minimum(r_hi, edges[1:, None])
 
-        return np.maximum(r_maxs - r_mins, 0) / (grid[1] - grid[0])
+        return np.maximum(r_maxs - r_mins, 0) / (edges[1] - edges[0])
     
     @staticmethod
     def _shapiro_filter(data: np.ndarray) -> np.ndarray:
@@ -290,8 +269,8 @@ class MeanFlow:
         Returns
         -------
         np.ndarray
-            Filtered array. Has two fewer elements than the data passed in.
-        
+            Filtered array. Has two fewer elements than the array passed in.
+
         """
 
         return (data[:-2] + 2 * data[1:-1] + data[2:]) / 4

msgwam/rays.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
-from copy import copy
-from typing import TYPE_CHECKING, Any, Optional, overload
+from typing import TYPE_CHECKING, Any
 
 import numpy as np
 
-from . import config, sources
-from .constants import RAD_EARTH
+from . import config
 
 if TYPE_CHECKING:
     from .mean import MeanFlow
@@ -19,61 +17,27 @@ class RayCollection:
     dk: np.ndarray; dl: np.ndarray; dm: np.ndarray
     dens: np.ndarray; age: np.ndarray; meta: np.ndarray
 
-    def __init__(self, mean: MeanFlow) -> None:
+    def __init__(self, sources: np.ndarray) -> None:
         """
-        Initialize a RayCollection based on loaded configuration settings and an
-        already-initialized MeanFlow.
+        Initialize a RayCollection based on the loaded config settings and an
+        array of source information.
 
         Parameters
         ----------
-        mean
-            MeanFlow to use for calcultion of sources.
+        sources
+            Array of source information. Should have nine rows corresponding
+            with the nine wave properties (excluding age and meta).
 
         """
-        
+
         shape = (len(self.props), config.n_ray_max)
         self.data = np.nan * np.zeros(shape)
+        self.sources = sources
         self.next_meta = -1
-
-        source_func = getattr(sources, config.source_type)
-        self.sources: np.ndarray = source_func(mean, self)
 
         self.ghosts = {}
         for slot, data in enumerate(self.sources.T):
-            self.ghosts[slot] = self.add_ray(*data)
-
-    def __add__(self, other: np.ndarray) -> RayCollection:
-        """
-        Return a new RayCollection object with the same source data but with ray
-        properties equal to those of this object added to the values in other.
-        Defined to make writing time-stepping routines easier.
-
-        Parameters
-        ----------
-        other
-            Array of data to be added to the ray properties (for example, their
-            time tendencies multiplied by the time step).
-
-        Returns
-        -------
-        RayCollection
-            RayCollection with updated ray properties.
-
-        Raises
-        ------
-        ValueError
-            Indicates that other does not have the correct shape; that is, the
-            shape of self.data.
-
-        """
-
-        if other.shape != self.data.shape:
-            raise ValueError('other does not have correct shape')
-
-        output = copy(self)
-        output.data = self.data + other
-
-        return output
+            self.ghosts[slot] = self.add_ray(data)
 
     def __getattr__(self, prop: str) -> Any:
         """
@@ -90,6 +54,7 @@ class RayCollection:
 
         Returns
         -------
+        np.ndarray
             Corresponding row of self.data.
 
         Raises
@@ -98,26 +63,12 @@ class RayCollection:
             Indicates that no ray property of the given name exists.
 
         """
-        
+
         if prop in self.indices:
             return self.data[self.indices[prop]]
 
         message = f'{type(self).__name__} object has no attribute {prop}'
         raise AttributeError(message)
-    
-    @property
-    def action(self) -> np.ndarray:
-        """
-        Calculate the wave action density.
-
-        Returns
-        -------
-            Array of wave action densities, calculated as the spectral wave
-            action multiplied by the spectral volume of each ray.
-
-        """
-
-        return self.dens * abs(self.dk * self.dl * self.dm)
 
     @property
     def valid(self) -> np.ndarray:
@@ -126,98 +77,128 @@ class RayCollection:
 
         Returns
         -------
-            Boolean array indicating whether a given column of self.data is
+        np.ndarray
+            Boolean array indicating whether each column of the array data is
             tracking an active ray volume or is free to be written over.
-        
+
         """
 
-        return ~(np.isnan(self.data).sum(axis=0) > 0)
+        return np.isnan(self.data).sum(axis=0) == 0
 
     @property
     def count(self) -> int:
         """
-        Count the number of active ray volumes in the collection
+        Count the number of active ray volumes in the collection.
 
         Returns
         -------
         int
-            Number of active ray volumes propagating.
+            Number of active ray volumes.
 
         """
 
         return self.valid.sum()
+    
+    @property
+    def action(self) -> int:
+        """
+        Calculate the wave action density.
 
-    def add_ray(
-        self,
-        r: float, dr: float,
-        k: float, l: float, m: float,
-        dk: float, dl: float, dm: float,
-        dens: float
-    ) -> int:
+        Returns
+        -------
+        np.ndarray
+            Array of wave action densities, calculated as the spectral wave
+            action multiplied by the spectral volume of each ray.
+
+        """
+
+        return self.dens * abs(self.dk * self.dl * self.dm)
+    
+    def add_ray(self, data: np.ndarray) -> int:
         """
         Add a ray to the collection, storing its data in the first available
-        column of self.data. If the RayCollection already has the maximum
-        allowable number of active ray volumes, throws an error.
+        column. Raises an error if the collection is already at the maximum
+        allowable number of active ray volumes.
 
         Parameters
         ----------
-        r
-            Vertical position of ray volume center.
-        dr
-            Vertical extent of ray volume.
-        k, l, m
-            Zonal, meridional, and vertical wavenumbers.
-        dk, dl, dm
-            Extent in zonal, meridional, and vertical wavenumber space.
-        dens
-            Wave action spectral density of ray volume.
+        data
+            Vector of ray properties (r, dr, k, l, m, dk, dl, dm, dens).
 
         Returns
         -------
         int
-            Index of the column of self.data where the new ray volume was added.
+            Index of the column where the new ray volume was added.
+
+        Raises
+        ------
+        RuntimeError
+            Indicates that the RayCollection already has config.n_ray_max rays.
 
         """
 
         if self.count == config.n_ray_max:
             raise RuntimeError('RayCollection has too many rays')
-
-        i = int(np.argmin(self.valid))
+        
+        j = int(np.argmin(self.valid))
         self.next_meta = self.next_meta + 1
+        self.data[:, j] = [*data, 0, self.next_meta]
 
-        self.data[:, i] = np.array([
-            r, dr,
-            k, l, m,
-            dk, dl, dm,
-            dens, 0, self.next_meta
-        ])
-
-        return i
-
-    def delete_rays(self, i: int | np.ndarray) -> None:
+        return j
+    
+    def delete_rays(self, j: int | np.ndarray) -> None:
         """
-        Mark the columns of self.data indicated by i as no longer corresponding
-        to active ray volumes.
+        Delete a ray volume by filling the corresponding column with np.nan.
 
         Parameters
         ----------
-        i
-            Index or array of indices of ray volumes to stop tracking.
+        j
+            Index or array of indices of ray volumes to delete.
 
         """
 
-        self.data[:, i] = np.nan
+        self.data[:, j] = np.nan
 
+    def omega_hat(self) -> np.ndarray:
+        """
+        Calculate the intrinsic frequencies of each ray in the collection. This
+        function is just a shorthand for calling _omega_hat with the appropriate
+        wave properties stored in this object.
+
+        Returns
+        -------
+        np.ndarray
+            Array of intrinsic frequencies. Will be np.nan at indices where no
+            ray volume is propagating.
+
+        """
+
+        return _omega_hat(self.k, self.l, self.m)
+    
+    def cg_r(self) -> np.ndarray:
+        """
+        Calculate the vertical group velocities for each ray in the collection.
+        This function is just a shorthand for calling _omega_hat with the
+        appropriate wave properties stored in this object.
+
+        Returns
+        -------
+        np.ndarray
+            Array of vertical group velocities. Will be np.nan at indices where
+            no ray volume is propagating.
+
+        """
+
+        return _cg_r(self.k, self.l, self.m)
+    
     def check_boundaries(self, mean: MeanFlow) -> None:
         """
-        Delete rays that have traveled below the surface or above the maximum of
-        the vertical grid. Then, add ray volumes as necessary to replace those
-        that are now fully above the ghost level.
-
+        Delete rays that have strayed outside of the physical domain.
+        
         Parameters
         ----------
         mean
-            MeanFlow from which to draw the vertical grid.
+            MeanFlow providing the vertical extent of the system.
 
         """
 
@@ -225,16 +206,19 @@ class RayCollection:
         above = self.r + 0.5 * self.dr > mean.r_faces[-1]
         self.delete_rays(below | above)
 
+    def check_source(self) -> None:
+        """
+        Enforce the bottom boundary condition by adding ray volumes as necessary
+        to replace those that have cleared the ghost layer.
+        """
+
         crossed = []
         for slot in range(self.sources.shape[1]):
             i = self.ghosts[slot]
             if self.r[i] - 0.5 * self.dr[i] > config.r_ghost:
                 crossed.append(slot)
 
-        size = int(config.epsilon * len(crossed))
-        to_launch = np.random.choice(crossed, size, replace=False)
-        excess = self.count + size - config.n_ray_max
-
+        excess = self.count + len(crossed) - config.n_ray_max
         if excess > 0:
             idx = np.argsort(self.action)
             exclude = list(self.ghosts.values())
@@ -242,118 +226,54 @@ class RayCollection:
 
             self.delete_rays(idx[:excess])
 
-        for slot in to_launch:
+        for slot in crossed:
             data = self.sources[:, slot].copy()
-            self.ghosts[slot] = self.add_ray(*data)
-
-    def omega_hat(
-        self,
-        k: Optional[float | np.ndarray]=None,
-        l: Optional[float | np.ndarray]=None,
-        m: Optional[float | np.ndarray]=None
-    ) -> float | np.ndarray:
+            self.ghosts[slot] = self.add_ray(data)
+    
+    def d_dt(self, mean: MeanFlow) -> np.ndarray:
         """
-        Calculate the intrinisic frequency of internal gravity waves.
-
-        Parameters
-        ----------
-        k, optional
-        l, optional
-        m, optional
-            Zonal, meridional, and vertical wavenumbers to use in the frequency
-            calculation. If any of these is None, the corresponding wavenumbers
-            from the rays in this collection will be used, such that calling
-            `rays.omega_hat()` with no arguments returns the intrinsic frequency
-            of each ray volume in the collection.
-
-        Returns
-        -------
-        float or np.ndarray
-            Intrinsic frequency. Only returns a float if all three of k, l, and
-            m are passed in as floats (e.g. during initialization).
-
-        """
-
-        k = self.k if k is None else k
-        l = self.l if l is None else l
-        m = self.m if m is None else m
-
-        return np.sqrt(
-            (config.N0 ** 2 * (k ** 2 + l ** 2) + config.f0 ** 2 * m ** 2) /
-            (k ** 2 + l ** 2 + m ** 2)
-        )
-
-    @overload
-    def cg_r(self, r: float, k: float, l: float, m: float) -> float:
-        ...
-
-    @overload
-    def cg_r(
-        self,
-        r: Optional[np.ndarray]=...,
-        k: Optional[np.ndarray]=...,
-        l: Optional[np.ndarray]=...,
-        m: Optional[np.ndarray]=...,
-    ) -> np.ndarray:
-        ...
-
-    def cg_r(self, r=None, k=None, l=None, m=None):
-        """
-        Calculate the vertical group velocity.
-
-        Parameters
-        ----------
-        r, optional
-            Vertical position of each wave. If None, the vertical position of
-            the waves in the collection will be used.
-        k, optional
-        l, optional
-        m, optional
-            Zonal, meridional, and vertical wavenumber of each wave. If any of
-            these is None, the corresponding wavenumbers of the waves in the
-            collection will be used.
-
-        Returns
-        -------
-        float or np.ndarray
-            Vertical group velocity of each wave. Only returns a float if all of
-            r, k, l, and m are passed explicitly as floats.
-
-        """
-
-        r = self.r if r is None else r
-        k = self.k if k is None else k
-        l = self.l if l is None else l
-        m = self.m if m is None else m
-
-        wvn_sq = k ** 2 + l ** 2 + m ** 2
-        omega_hat = self.omega_hat(k, l, m)
-
-        return -m * (
-            (omega_hat ** 2 - config.f0 ** 2) /
-            (omega_hat * wvn_sq)
-        )
-
-    def dm_dt(self, mean: MeanFlow) -> np.ndarray:
-        """
-        Calculate the time tendency of the vertical wavenumber.
+        Calculate the time tendency of each ray property.
 
         Parameters
         ----------
         mean
-            MeanFlow from which to calculate vertical wind gradients.
+            Current mean state of the system.
 
         Returns
         -------
         np.ndarray
-            Time tendency of the vertical wavenumber of waves in the collection.
+            Array of time tendencies, each row of which corresponds to the ray
+            property named in self.props.
 
         """
 
+        dr_dt = self.cg_r()
         du_dr = np.interp(self.r, mean.r_faces[1:-1], np.diff(mean.u) / mean.dr)
         dv_dr = np.interp(self.r, mean.r_faces[1:-1], np.diff(mean.v) / mean.dr)
+        dm_dt = -(self.k * du_dr + self.l * dv_dr)
+        
+        ddr_dt, ddm_dt = np.zeros((2, config.n_ray_max))
+        dk_dt, dl_dt, ddk_dt, ddl_dt = np.zeros((4, config.n_ray_max))
 
-        return -(self.k * du_dr + self.l * dv_dr)
+        omega_hat = self.omega_hat()
+        nu = np.interp(self.r, mean.r_faces, mean.nu)
+        wvn_sq = self.k ** 2 + self.l ** 2 + self.m ** 2
+
+        damping = nu * wvn_sq * (1 + config.f0 ** 2 / omega_hat ** 2)
+        ddens_dt = -damping * config.dissipation * self.dens
+
+        dage_dt = np.ones(config.n_ray_max)
+        dmeta_dt = np.zeros(config.n_ray_max)
+
+        idx = self.r < config.r_launch
+        dm_dt[idx] = ddr_dt[idx] = ddm_dt[idx] = 0
+
+        return np.vstack((
+            dr_dt, ddr_dt,
+            dk_dt, dl_dt, dm_dt,
+            ddk_dt, ddl_dt, ddm_dt,
+            ddens_dt, dage_dt, dmeta_dt
+        ))
     
     def break_waves(self, mean: MeanFlow) -> None:
         """
@@ -369,7 +289,7 @@ class RayCollection:
 
         omega_hat = self.omega_hat()
         wvn_sq = self.k ** 2 + self.l ** 2 + self.m ** 2
-        
+
         S = self.m ** 2 * omega_hat * self.action
         P = mean.project(self, S, 'centers') - mean.rho * config.N0 ** 2 / 2
         Q = mean.project(self, S * wvn_sq, 'centers')
@@ -383,65 +303,56 @@ class RayCollection:
 
         factor = 1 - wvn_sq * np.max(intersects * kappa[:, None], axis=0)
         self.dens[:] = self.dens * factor
-
-    def drays_dt(self, mean: MeanFlow) -> np.ndarray:
-        """
-        Calculate the time tendency of each ray property.
-
-        Parameters
-        ----------
-        mean
-            Current mean state of the system.
-
-        Returns
-        -------
-        np.ndarray
-            Array of time tendencies, each row of which corresponds to the ray
-            property named in self.props.
-
-        Raises
-        ------
-        NotImplementedError
-            Indicates that horizontal propagation was turned on in the config.
-            
-        """
-
-        cg_r_down = self.cg_r(self.r - 0.5 * self.dr)
-        cg_r_up = self.cg_r(self.r + 0.5 * self.dr)
-        dr_dt = (cg_r_down + cg_r_up) / 2
-        ddr_dt = cg_r_up - cg_r_down
-
-        dk_dt = np.zeros(config.n_ray_max)
-        dl_dt = np.zeros(config.n_ray_max)
-        ddk_dt = np.zeros(config.n_ray_max)
-        ddl_dt = np.zeros(config.n_ray_max)
-
-        dm_dt = self.dm_dt(mean)
-        ddm_dt = ddr_dt * self.dm / self.dr
-
-        omega_hat = self.omega_hat()
-        nu = np.interp(self.r, mean.r_faces, mean.nu)
-        wvn_sq = self.k ** 2 + self.l ** 2 + self.m ** 2
-
-        damping = nu * wvn_sq * (1 + config.f0 ** 2 / omega_hat ** 2)
-        ddens_dt = -damping * config.dissipation * self.dens
         
-        dage_dt = np.ones(config.n_ray_max)
-        dmeta_dt = np.zeros(config.n_ray_max)
+def _omega_hat(k: np.ndarray, l: np.ndarray, m: np.ndarray) -> np.ndarray:
+    """
+    Calculate the intrinsic frequency of internal gravity waves.
 
-        idx = self.r < config.r_launch
-        dm_dt[idx] = ddr_dt[idx] = ddm_dt[idx] = 0
+    Parameters
+    ----------
+    k
+        Array of zonal wavenumbers.
+    l
+        Array of meridional wavenumbers.
+    m
+        Array of vertical wavenumbers.
 
-        return np.vstack((
-            dr_dt,
-            ddr_dt,
-            dk_dt,
-            dl_dt,
-            dm_dt,
-            ddk_dt,
-            ddl_dt,
-            ddm_dt,
-            ddens_dt,
-            dage_dt,
-            dmeta_dt
-        ))
+    Returns
+    -------
+    np.ndarray
+        Array of intrinsic frequencies.
+
+    """
+
+    return np.sqrt(
+        (config.N0 ** 2 * (k ** 2 + l ** 2) + config.f0 ** 2 * m ** 2) /
+        (k ** 2 + l ** 2 + m ** 2)
+    )
+
+def _cg_r(k: np.ndarray, l: np.ndarray, m: np.ndarray) -> np.ndarray:
+    """
+    Calculate the vertical group velocity of internal gravity waves.
+
+    Parameters
+    ----------
+    k
+        Array of zonal wavenumbers.
+    l
+        Array of meridional wavenumbers.
+    m
+        Array of vertical wavenumbers.
+
+    Returns
+    -------
+    np.ndarray
+        Array of vertical group velocities.
+
+    """
+
+    wvn_sq = k ** 2 + l ** 2 + m ** 2
+    omega_hat = _omega_hat(k, l, m)
+
+    return -m * (
+        (omega_hat ** 2 - config.f0 ** 2) /
+        (omega_hat * wvn_sq)
+    )

msgwam/sources.py
@@ -4,17 +4,16 @@ from typing import TYPE_CHECKING
 import numpy as np
 
 from . import config
+from .rays import _cg_r, _omega_hat
 
 if TYPE_CHECKING:
     from .mean import MeanFlow
-    from .rays import RayCollection
 
-# Each function in this module should accept a MeanFlow and a RayCollection and
-# return an array with rows corresponding to the ray properties named in
-# RayCollection.props and columns corresponding to however many ray volumes
-# should be launched at the source.
+# Each function in this module should accept a MeanFlow and return an array with
+# rows corresponding to the ray properties named in RayCollection.props and
+# columns corresponding the ray volumes that should be launched at the source.
 
-def desaubies(mean: MeanFlow, rays: RayCollection) -> np.ndarray:
+def desaubies(mean: MeanFlow) -> np.ndarray:
     """
     Calculate source ray volume properties according to the Desaubies spectrum
     as defined in Boloni et al. (2021). The number of ray volumes is determined
@@ -53,7 +52,7 @@ def desaubies(mean: MeanFlow, rays: RayCollection) -> np.ndarray:
     C = config.bc_mom_flux / (rhobar * G.sum() * dct * dot)
 
     n_each = config.n_c_tilde * config.n_omega_tilde
-    data = np.zeros((len(rays.props) - 2, 4 * n_each))
+    data = np.zeros((9, 4 * n_each))
 
     r = (config.r_ghost - 0.5 * config.dr_init) * np.ones(n_each)
     dr = config.dr_init * np.ones(n_each)
@@ -69,7 +68,7 @@ def desaubies(mean: MeanFlow, rays: RayCollection) -> np.ndarray:
         if i % 2 == 1:
             dk, dl = dl, dk
 
-        cg_r = rays.cg_r(r=r, k=k, l=l, m=m)
+        cg_r = _cg_r(r=r, k=k, l=l, m=m)
         dens = (
             (rhobar * C * G * c_tilde ** 5) /
             (config.N0 * omega_tilde ** 2 * cg_r)
@@ -80,7 +79,7 @@ def desaubies(mean: MeanFlow, rays: RayCollection) -> np.ndarray:
 
     return data
 
-def legacy(mean: MeanFlow, rays: RayCollection) -> np.ndarray:
+def legacy(mean: MeanFlow) -> np.ndarray:
     """
     Calculate source ray volumes as was done in the original version of this
     Python code. Note that the library defaults to not launching more rays at
@@ -104,7 +103,7 @@ def legacy(mean: MeanFlow, rays: RayCollection) -> np.ndarray:
     dm = config.r_m_area / dr
 
     rhobar = np.interp(r, mean.r_centers, mean.rho)
-    omega_hat = rays.omega_hat(k=k, l=l, m=m)
+    omega_hat = _omega_hat(k=k, l=l, m=m)
 
     amplitude = (
         (config.alpha ** 2 * rhobar * omega_hat * config.N0 ** 2) /
@@ -116,7 +115,7 @@ def legacy(mean: MeanFlow, rays: RayCollection) -> np.ndarray:
 
     return np.vstack((r, dr, k, l, m, dk, dl, dm, dens))
 
-def gaussians(_, rays: RayCollection) -> np.ndarray:
+def gaussians(_) -> np.ndarray:
     dr = config.dr_init
     r = config.r_ghost - 0.5 * dr
 
@@ -143,7 +142,7 @@ def gaussians(_, rays: RayCollection) -> np.ndarray:
     data = np.zeros((9, config.n_source))
     for j, (m, dm, flux) in enumerate(zip(ms, dms, fluxes)):
         volume = abs(dk * dl * dm)
-        cg_r = rays.cg_r(r=r, k=k, l=l, m=m)
+        cg_r = _cg_r(k=k, l=l, m=m)
         dens = flux / abs(k * volume * cg_r)
 
         data[:, j] = np.array([r, dr, k, l, m, dk, dl, dm, dens])