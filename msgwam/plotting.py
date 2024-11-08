first pass at reimplementing the stochastic source, although average boundary flux is not quite correct

msgwam/plotting.py
@@ -17,6 +17,45 @@ if TYPE_CHECKING:
     from matplotlib.collections import QuadMesh
     from matplotlib.colorbar import Colorbar
 
+def plot_boundary(ds: xr.Dataset, output_path: str) -> None:
+    """
+    Plot the flux at the bottom boundary over time.
+
+    Parameters
+    ----------
+    Parameters
+    ----------
+    ds
+        Dataset containing the integration output.
+    output_path
+        Where to save the image.
+
+    """
+
+    fig, ax = plt.subplots()
+    fig.set_size_inches(4.5, 3)
+
+    days = config.dt * np.arange(config.n_steps) / 86400
+    pmf = 1000 * (ds['pmf_e'] - ds['pmf_w']).isel(z_faces=0)
+    line = 1000 * config.flux_bc * np.ones_like(days)
+
+    ax.plot(days, pmf, color='k')
+    ax.plot(days, line, color='gray', ls='dashed')
+
+    ax.set_xlim(0, days.max())
+    ax.set_ylim(0, 5)
+
+    ax.set_xlabel('time (days)')
+    ax.set_ylabel('boundary flux (mPa)')
+    ax.set_title(f'mean flux = {pmf.mean():.2f} mPa')
+    
+    ax.set_axisbelow(True)
+    ax.grid(color='lightgray')
+    ax.tick_params('both', direction='in')
+
+    plt.tight_layout()
+    plt.savefig(output_path, dpi=400)
+
 def plot_integration(ds: xr.Dataset, output_path: str) -> None:
     """
     Make a summary plot of an integration, including the mean wind and total,
@@ -50,7 +89,7 @@ def plot_integration(ds: xr.Dataset, output_path: str) -> None:
 
     names = ['total', 'westerly', 'easterly']
     pmfs = [ds['pmf_e'] + ds['pmf_w'], ds['pmf_e'], ds['pmf_w']]
-    amax = np.ceil(1000 * pmfs[1].max())
+    amax = np.ceil(1000 * (pmfs[1].max() + 2 * pmfs[1].std()))
 
     for name, pmf, ax in zip(names, pmfs, axes[1:]):
         _, cbar = plot_time_series(1000 * pmf, amax, [ax, caxes[1]])
@@ -143,7 +182,7 @@ def plot_source(output_path: str) -> None:
     cbar.set_ticks(np.linspace(0, vmax, 5))
     cbar.set_label('flux (mPa)')
 
-    vmax = 3.6 * flux.max()
+    vmax = 3.6 * cg_r.max()
     vmax = np.ceil(vmax / 5) * 5
 
     img = axes[1].pcolormesh(

msgwam/propagators/transient.py
@@ -118,6 +118,9 @@ class TransientPropagator(Propagator):
         data = np.vstack([wvn * action_flux for wvn in wvns])
         fluxes = self._project(data, self._z_padded)
 
+        if config.source_type == 'stochastic':
+            fluxes = fluxes / config.epsilon
+
         if config.shapiro_filter:
             fluxes[:, 1:-1] = shapiro_filter(fluxes.T).T
 
@@ -290,9 +293,10 @@ class TransientPropagator(Propagator):
         excess = self.n_active + datas.shape[1] - self._n_max
 
         jdx = self._ghosts[cdx]
-        r_hi = self.r[jdx] + 0.5 * self.dr[jdx]
-        self._data[0, jdx] = (config.z_min + r_hi) / 2
-        self._data[1, jdx] = r_hi - config.z_min
+        if config.source_type != 'stochastic':
+            r_hi = self.r[jdx] + 0.5 * self.dr[jdx]
+            self._data[0, jdx] = (config.z_min + r_hi) / 2
+            self._data[1, jdx] = r_hi - config.z_min
 
         self._prune(excess, mean)
         for k, data in zip(cdx, datas.T):

msgwam/sources/base.py
@@ -56,18 +56,18 @@ class Source(FactoryABC):
         waves that should be launched.
 
         Even constant-in-time spectra may have vertical wavenumbers, extents,
-        and spectral densities that vary in time if the buoyancy frequency is
-        not constant. This function therefore also calculates those properties
-        and includes them in the returned data.
+        and spectral densities that vary in time if the mean wind or buoyancy
+        frequency are not constant. This function therefore also calculates
+        those properties and includes them in the returned data.
 
         Because some sources (e.g. stochastic ones) might not return as many
         waves as were requested, we also return an array indicating which
         requested wave each returned wave corresponds to.
 
-        This is the public method meant to be called by propagators. This method
-        relies on the `_launch` method implemented by subclasses, which handles
-        the actual launch logic particular to each source. This method than
-        calculates the additional wave properties mentioned above.
+        This is the public method meant to be called by propagators, and here we
+        derive the time-varying wave properties mentioned above. We then rely on
+        the `_postprocess` method implemented by subclasses to handle the launch
+        logic particular to each source type.
 
         Parameters
         ----------
@@ -93,7 +93,7 @@ class Source(FactoryABC):
         if cdx is None:
             cdx = np.arange(config.n_source)
 
-        (k, l, dk, dl, flux), cdx = self._launch(mean, n_step, cdx)
+        k, l, dk, dl, flux = self._data[n_step][:, cdx]
         m = get_m(k, l, self._cp_x[cdx] - mean.u[0], mean.N[0])
         dm = get_dm(m, self.dc, mean.N[0])
 
@@ -101,18 +101,18 @@ class Source(FactoryABC):
         dens = flux / abs(k * dk * dl * dm * cg_r)
         data = np.vstack((k, l, m, dk, dl, dm, dens))
 
-        return data, cdx
+        return self._postprocess(mean, cg_r, data, cdx)
 
     @abstractmethod
-    def _launch(
+    def _postprocess(
         self,
         mean: MeanState,
-        n_step: int,
-        cdx: np.ndarray
+        cg_r: np.ndarray,
+        data: np.ndarray,
+        cdx: np.ndarray,
     ) -> tuple[np.ndarray, np.ndarray]:
         """
-        Subclass-specific source logic called by `launch`. See the docstring for
-        that method for more details on parameters and return values.
+        Apply any source-specific logic to the selected wave properties. See the
+        docstring for `launch` for more details on the return values.
         """
         ...
-

msgwam/sources/deterministic.py
@@ -1,18 +1,24 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
 import numpy as np
 
 from .base import Source
 
+if TYPE_CHECKING:
+    from ..means import MeanState
+
 class DeterministicSource(Source):
-    def _launch(
+    def _postprocess(
         self,
-        _, 
-        n_step: int,
+        mean: MeanState,
+        cg_r: np.ndarray,
+        data: np.ndarray,
         cdx: np.ndarray
     ) -> tuple[np.ndarray, np.ndarray]:
         """
-        A deterministic source returns the properties of all requested waves,
-        along with the required second return value indicating as much.
+        A deterministic source returns the properties of all requested waves
+        unchanged, along with the required second array indicating as much.
         """
 
-        return self._data[n_step][:, cdx], cdx
-        
+        return data, cdx

msgwam/sources/spectra.py
@@ -114,7 +114,7 @@ def _gaussians() -> xr.Dataset:
     flux = config.flux_bc * flux / flux.sum()
 
     ones = np.ones_like(cp_x)
-    spectrum = np.vstack((k, l * ones, dk * ones, dl * ones, flux))
+    spectrum = np.vstack((k, l, dk * ones, dl * ones, flux))
 
     data: dict[str, Any] = {'cp_x' : cp_x}
     for i, name in enumerate(['k', 'l', 'dk', 'dl', 'flux']):