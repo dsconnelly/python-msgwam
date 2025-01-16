387ea1c3b7dc760d047a279ae1ba20165854cfa5
dsconnelly <dsconnelly@nyu.edu>
2025-01-16 17:13:56 -0500

    fix to coarsening overrides

--- experiments/ml-accel/evaluation/coarsening.py
+++ experiments/ml-accel/evaluation/coarsening.py
@@ -18,9 +18,12 @@ def save_coarsenings() -> None:
     `config.n_max`, saving the output of each configuration.
     """
 
-    base = _get_stochastic_overrides('1')
+    overrides = _get_stochastic_overrides('1')
     for dr, n_source in product(*_get_grid()):
-        with config.override(**base, dr_init=float(dr), n_source=n_source):
+        overrides['dr_init'] = float(dr)
+        overrides['n_source'] = n_source
+
+        with config.override(**overrides):
             _get_integration('stochastic').to_netcdf(_get_path(dr, n_source))
 
 def update_config(*names: str) -> None:
