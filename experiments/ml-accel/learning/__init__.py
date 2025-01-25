280d7308f19c3b9e0490edd7e8237677ac3cc708
dsconnelly <dsconnelly@nyu.edu>
2025-01-25 13:55:57 -0500

    removed a reference to an undefined plotting function

--- experiments/ml-accel/learning/__init__.py
+++ experiments/ml-accel/learning/__init__.py
@@ -1,9 +1,4 @@
-from .plotting import (
-    plot_cv_scores,
-    plot_flux_summary,
-    plot_network_errors,
-    plot_training_samples
-)
+from .plotting import plot_cv_scores, plot_network_errors, plot_training_samples
 from .preprocessing import (
     invert_surrogate,
     save_proxies,
@@ -17,7 +12,6 @@ __all__ = [
     'combine_data',
     'invert_surrogate',
     'plot_cv_scores',
-    'plot_flux_summary',
     'plot_network_errors',
     'plot_training_samples',
     'save_proxies',
