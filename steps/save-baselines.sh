3a81e9902be613c5e242ee6efcaebcf70c7ace6c
dsconnelly <dsconnelly@nyu.edu>
2025-01-31 13:22:45 -0500

    fix in baselines submission script

--- steps/save-baselines.sh
+++ steps/save-baselines.sh
@@ -7,14 +7,14 @@ job_id=$(sbatch \
     --parsable \
     --ntasks=1 \
     --mem=32G \
-    --time=1:00:00 \
-    --array=0-99 \
-    -J coarsening \
+    --time=3:00:00 \
+    -J baselines \
     -o logs/$name/baselines.out \
     $dep_arg \
     submit.slurm config/$name.toml \
-        save-integration:stochastic-1 \
-        save-integration:stochastic-25 \
+        save-integration:coarse \
+        save-integration:stochastic:1 \
+        save-integration:stochastic:25 \
         save-integration:instantaneous
 )
 
