aab85b0161ca79b8f043cb83b670ec180553383e
dsconnelly <dsconnelly@nyu.edu>
2025-01-31 13:50:13 -0500

    made plot_error_profiles usable from command line

--- acceleration/strategies/__init__.py
+++ acceleration/strategies/__init__.py
@@ -1,10 +1,11 @@
 from .coarsenings import save_coarsenings, update_config
 from .integration import get_integration, save_integration
-from .plotting import plot_coarse_errors, plot_strategy
+from .plotting import plot_coarse_errors, plot_error_profiles, plot_strategy
 
 __all__ = [
     'get_integration',
     'plot_coarse_errors',
+    'plot_error_profiles',
     'plot_strategy',
     'save_coarsenings',
     'save_integration',
