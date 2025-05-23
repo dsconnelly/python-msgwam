ad9ff9b0a1872895994c15fb97b9e6005dd0d56c
dsconnelly <dsconnelly@nyu.edu>
2025-05-23 10:37:20 -0400

    forgot to make it 30 days

--- scripts/save-reference.sh
+++ scripts/save-reference.sh
@@ -14,13 +14,15 @@ job_id=$(sbatch \
     --ntasks=1 \
     --mem=128G \
     --cpus-per-task=8 \
-    --time=24:00:00 \
+    --time=18:00:00 \
     -J reference \
     -o logs/$name/reference.out \
     $dep_arg \
     submit.slurm config/$name.toml \
         $arg \
         save-spectrum \
+        plot-spectrum \
+        plot-mean-state \
         save-strategy:reference \
         plot-strategy:reference
 )
